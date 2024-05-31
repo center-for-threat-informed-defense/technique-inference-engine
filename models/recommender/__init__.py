8a3c0cb6692f8e2ba8fff266708ffd3a4a655e92
mturner-ml <46545771+mturner-ml@users.noreply.github.com>
2024-05-31 15:22:44 -0400

    Matt/implicit bpr recommender (#20)
    * implement implicit bpr recommender
    
    * clean up code
    
    * add spec
    
    ---------
    
    Co-authored-by: Turner <mjturner@mitre.org>

--- models/recommender/__init__.py
+++ models/recommender/__init__.py
@@ -1,11 +1,13 @@
 from .factorization_recommender import FactorizationRecommender
 from .bpr_recommender import BPRRecommender
+from .implicit_bpr_recommender import ImplicitBPRRecommender
 from .wals_recommender import WALSRecommender
 from .recommender import Recommender
 
 __all__ = [
     "FactorizationRecommender",
     "BPRRecommender",
+    "ImplicitBPRRecommender",
     "WALSRecommender",
     "Recommender",
 ]
--- models/recommender/implicit_bpr_recommender.py
+++ models/recommender/implicit_bpr_recommender.py
@@ -1,37 +1,39 @@
+import math
+from .recommender import Recommender
 import numpy as np
 import tensorflow as tf
-from .recommender import Recommender
-from implicit.als import AlternatingLeastSquares
-from sklearn.metrics import mean_squared_error
+from implicit.bpr import BayesianPersonalizedRanking
 from scipy import sparse
-import copy
+from sklearn.metrics import mean_squared_error
 
 
-class WALSRecommender(Recommender):
-    """A WALS matrix factorization collaborative filtering recommender model."""
+class ImplicitBPRRecommender:
+    """A matrix factorization recommender model to suggest items for a particular entity."""
 
     # Abstraction function:
-    # AF(model, m, n, k, num_new_users) = a matrix factorization collaborative filtering recommendation model
-    #   of embedding dimension k with m entity embeddings model.user_factors
-    #   and n item embeddings model.item_factors.
-    #   The model has performed cold start prediction for num_new_users.
+    #   AF(m, n, k, model, num_new_users) = model to be trained with embedding dimension k
+    #       on m entities and n items if model is None,
+    #       or model trained on such data and with predictions for num_new_users
+    #       if model is not None
     # Rep invariant:
     #   - m > 0
     #   - n > 0
     #   - k > 0
+    #   - num_new_users >= 0
     # Safety from rep exposure:
     #   - k is private and immutable
     #   - model is never returned
 
-    def __init__(self, m: int, n: int, k: int = 10):
-        """Initializes a new WALSRecommender object.
+    def __init__(self, m: int, n: int, k: int):
+        """Initializes an ImplicitBPRRecommender object.
 
         Args:
             m: number of entities.  Requires m > 0.
             n: number of items.  Requires n > 0.
-            k: embedding dimension.  Requires k > 0.
+            k: the embedding dimension.  Requires k > 0.
         """
-        assert m > 0
+        # assert preconditions
+        assert k > 0
         assert n > 0
         assert k > 0
 
@@ -40,7 +42,6 @@ class WALSRecommender(Recommender):
         self._k = k
         self._model = None
 
-        # for tracking how many new users we've seen so far
         self._num_new_users = 0
 
         self._checkrep()
@@ -53,6 +54,8 @@ class WALSRecommender(Recommender):
         assert self._n > 0
         #   - k > 0
         assert self._k > 0
+        #   - num_new_users >= 0
+        assert self._num_new_users >= 0
 
     @property
     def U(self) -> np.ndarray:
@@ -73,29 +76,31 @@ class WALSRecommender(Recommender):
     def fit(
         self,
         data: tf.SparseTensor,
-        num_iterations: int = 20,
-        c: float = 0.024,
-        regularization_coefficient: float = 0.01,
+        learning_rate: float,
+        num_iterations: int,
+        regularization: float,
+        **kwargs,
     ):
         """Fits the model to data.
 
         Args:
             data: an mxn tensor of training data.
+            learning_rate: the learning rate.
+                Requires learning_rate > 0.
             num_iterations: number of training iterations to execute.
-            c: weight for negative training examples.  Requires 0 < c < 1.
-            regularization_coefficient: coefficient on the embedding regularization term.
+                Requires num_iterations > 0.
+            regularization: coefficient on the embedding regularization term.
 
         Mutates:
             The recommender to the new trained state.
         """
-        assert 0 < c < 1
 
-        alpha = (1 / c) - 1
-        self._model = AlternatingLeastSquares(
+        self._model = BayesianPersonalizedRanking(
             factors=self._k,
-            regularization=regularization_coefficient,
+            learning_rate=learning_rate,
+            regularization=regularization,
             iterations=num_iterations,
-            alpha=alpha,
+            verify_negative_samples=True,
         )
 
         row_indices = tuple(index[0] for index in data.indices)
@@ -143,30 +148,5 @@ class WALSRecommender(Recommender):
         self._checkrep()
         return np.dot(self._model.user_factors, self._model.item_factors.T)
 
-    def predict_new_entity(self, entity: tf.SparseTensor) -> np.array:
-        """Recommends items to an unseen entity.
-
-        Args:
-            entity: a length-n sparse tensor of consisting of the new entity's
-                ratings for each item, indexed exactly as the items used to
-                train this model.
-
-        Returns:
-            An array of predicted values for the new entity.
-        """
-        # just need an item 0 for all entity indices
-        row_indices = np.zeros(len(entity.indices))
-        column_indices = entity.indices[:, 0]
-
-        sparse_data = sparse.csr_matrix(
-            (entity.values, (row_indices, column_indices)), shape=(1, entity.shape[0])
-        )
-
-        user_id = self._m + self._num_new_users
-
-        self._model.partial_fit_users((user_id,), sparse_data)
-
-        self._num_new_users += 1
-
-        self._checkrep()
-        return np.dot(self._model.user_factors[user_id], self._model.item_factors.T)
+    def predict_new_entity(self, entity: tf.SparseTensor, **kwargs) -> np.array:
+        raise NotImplementedError
