67f0e737d0eaae272f62fab4d98d90c787786abb
mturner-ml <46545771+mturner-ml@users.noreply.github.com>
2024-07-03 15:35:42 -0400

    implement top techniques recommender (#27)
    Co-authored-by: Turner <mjturner@mitre.org>

--- models/recommender/__init__.py
+++ models/recommender/__init__.py
@@ -3,6 +3,7 @@ from .bpr_recommender import BPRRecommender
 from .implicit_bpr_recommender import ImplicitBPRRecommender
 from .wals_recommender import WalsRecommender
 from .implicit_wals_recommender import ImplicitWalsRecommender
+from .top_items_recommender import TopItemsRecommender
 from .recommender import Recommender
 
 __all__ = [
@@ -11,5 +12,6 @@ __all__ = [
     "ImplicitBPRRecommender",
     "WalsRecommender",
     "ImplicitWalsRecommender",
+    "TopItemsRecommender",
     "Recommender",
 ]
