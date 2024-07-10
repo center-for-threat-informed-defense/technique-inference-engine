46422dd9527f12dc24a8be726e8b2adab9b94546
Michael Carenzo <79934822+mikecarenzo@users.noreply.github.com>
2024-07-10 11:59:51 -0400

    update about view layout

--- src/tie-web-interface/src/views/AboutView.vue
+++ src/tie-web-interface/src/views/AboutView.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="about-contents">
+      <h1>This is an about page</h1>
+    </div>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<style lang="scss" scoped>
+@use "@/assets/styles/engenuity_scaling_system.scss" as scale;
+
+.about {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  margin: scale.size("xxh") 0em;
+}
+
+.about-contents {
+  display: flex;
+  width: 100%;
+  max-width: scale.$max-width;
 }
 </style>
