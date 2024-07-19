47de3b07af3b3f0f2442ae4ab7ea2043d0cdfc7c
Michael Carenzo <79934822+mikecarenzo@users.noreply.github.com>
2024-07-19 15:14:16 -0400

    add example about page

--- src/tie-web-interface/src/views/AboutView.vue
+++ src/tie-web-interface/src/views/AboutView.vue
@@ -1,10 +1,138 @@
 <template>
   <div class="about">
     <div class="about-contents">
-      <h1>This is an about page</h1>
+      <h1>About</h1>
+      <p>
+        <b>Paragraph 1: Bold text, bold text, bold text</b> consectetur adipiscing elit. Aliquam aliquam nunc ac
+        leocommodo, <a href="www.google.com">a very important hyperlink.</a> nec fringilla augue viverra. Suspendisse
+        eleifend a ipsum ac imperdiet. Ut faucibus sollicitudin viverra. Sed id dapibus ligula. Fusce porta eros et arcu
+        efficitur rhoncus. Donec bibendum consequat eros, ut rutrum erat congue sed. Curabitur at rhoncus turpis. In
+        luctus nec neque sed bibendum. Curabitur volutpat leo ut fermentum dictum.
+      </p>
+      <p>
+        Notice that the block equation below is surrounded by <code>$$</code> while an inline equation like this one:
+        $f(x)=x^2 + x + 1$ is surrounded by <code>$</code>.
+        $$\mathrm{P = \sum\limits _{i=1}^{n} w( t_{now} -t_{i})}$$
+      </p>
+      <p>
+        <i>
+          Paragraph 2: Italicized text, Italicized text, Italicized text, Aliquam sit amet venenatis metus. Fusce sit
+          amet neque ac ante rutrum pharetra eget et arcu. Praesent sed tristique ante. Praesent a hendrerit orci, vel
+          blandit purus. Pellentesque dapibus ipsum massa, aliquam erat dapibus at. Quisque congue molestie erat, at
+          convallis metus convallis id. Phasellus ante massa, imperdiet et posuere quis, euismod et arcu. Integer ex
+          elit, tincidunt eu massa sit amet, pharetra tincidunt tortor.
+        </i>
+      </p>
+      <p>
+        Paragraph 3: <b><i>Bold AND Italicized text.</i></b>Fusce eget mauris interdum, ornare erat ut, interdum ipsum.
+        Integer elit justo, congue vulputate vestibulum quis, tincidunt scelerisque orci Curabitur eu euismod ipsum.
+        Mauris egestas cursus diam, et aliquam est consequat sit amet. Nam dui orci, convallis vitae justo in, euismod
+        finibus nisi. Pellentesque a congue sapien, ut gravida
+        <code>an inline code snippet</code> massa. Nulla bibendum gravida sem, et placerat dolor cursus non.
+      </p>
+      <h2>Section #1</h2>
+      <h3>Subsection #1</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Aliquam aliquam nunc ac leo commodo, nec fringilla
+        augue viverra. Suspendisse eleifend a ipsum ac imperdiet. Ut faucibus sollicitudin viverra. Sed id dapibus
+        ligula. Fusce porta eros et arcu efficitur rhoncus. Donec bibendum consequat eros, ut rutrum erat congue sed.
+        Curabitur at rhoncus turpis. In luctus nec neque sed bibendum. Curabitur volutpat leo ut fermentum dictum.
+      </p>
+      <p>
+        Quisque congue molestie erat, at convallis metus convallis id. Phasellus ante massa,
+        imperdiet et posuere quis, euismod et arcu. Integer ex elit, tincidunt eu massa sit amet, pharetra tincidunt
+        tortor. Quisque congue molestie erat, at convallis metus convallis id. Phasellus ante massa,
+        imperdiet et posuere quis, euismod et arcu. Integer ex elit, tincidunt eu massa sit amet, pharetra tincidunt
+        tortor.
+      </p>
+      <img src="/images/example_image.png" style="max-width:600px;">
+      <p>
+        Aliquam sit amet venenatis metus. Fusce sit amet neque ac ante rutrum pharetra eget et arcu.
+        Praesent sed tristique ante. Praesent a hendrerit orci, vel blandit purus. Pellentesque dapibus ipsum massa, id
+        aliquam erat dapibus at. Quisque congue molestie erat, at convallis metus convallis id. Phasellus ante massa,
+        imperdiet et posuere quis, euismod et arcu. Integer ex elit, tincidunt eu massa sit amet, pharetra tincidunt
+        tortor.
+      </p>
+      <ul>
+        <li>Unordered List Item #1</li>
+        <li>Unordered List Item #2</li>
+        <li>Unordered List Item #3</li>
+        <li>Unordered List Item #4</li>
+      </ul>
+      <p>
+        Aliquam sit amet venenatis metus. Fusce sit amet neque ac ante rutrum pharetra eget et arcu.
+        Praesent sed tristique ante. Praesent a hendrerit orci, vel blandit purus. Pellentesque dapibus ipsum massa, id
+        aliquam erat dapibus at. Quisque congue molestie erat, at convallis metus convallis id. Phasellus ante massa,
+        imperdiet et posuere quis, euismod et arcu. Integer ex elit, tincidunt eu massa sit amet, pharetra tincidunt
+        tortor.
+      </p>
+      <h3>Subsection #2</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam nunc ac leo commodo, nec fringilla
+        augue viverra. Suspendisse eleifend a ipsum ac imperdiet. Ut faucibus sollicitudin viverra. Sed id dapibus
+        ligula. Fusce porta eros et arcu efficitur rhoncus. Donec bibendum consequat eros, ut rutrum erat congue sed.
+        Curabitur at rhoncus turpis. In luctus nec neque sed bibendum. Curabitur volutpat leo ut fermentum dictum.
+      </p>
+      <ol>
+        <li>Ordered List Item #1</li>
+        <li>Ordered List Item #2</li>
+        <li>Ordered List Item #3</li>
+        <li>Ordered List Item #4</li>
+      </ol>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam nunc ac leo commodo, nec fringilla
+        augue viverra. Suspendisse eleifend a ipsum ac imperdiet. Ut faucibus sollicitudin viverra. Sed id dapibus
+        ligula. Fusce porta eros et arcu efficitur rhoncus. Donec bibendum consequat eros, ut rutrum erat congue sed.
+        Curabitur at rhoncus turpis. In luctus nec neque sed bibendum. Curabitur volutpat leo ut fermentum dictum.
+      </p>
+      <h2>Section #2</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam nunc ac leo commodo, nec fringilla
+        augue viverra. Suspendisse eleifend a ipsum ac imperdiet. Ut faucibus sollicitudin viverra. Sed id dapibus
+        ligula. Fusce porta eros et arcu efficitur rhoncus. Donec bibendum consequat eros, ut rutrum erat congue sed.
+        Curabitur at rhoncus turpis. In luctus nec neque sed bibendum. Curabitur volutpat leo ut fermentum dictum.
+      </p>
+      <h2>Section #3</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam nunc ac leo commodo, nec fringilla
+        augue viverra. Suspendisse eleifend a ipsum ac imperdiet. Ut faucibus sollicitudin viverra. Sed id dapibus
+        ligula. Fusce porta eros et arcu efficitur rhoncus. Donec bibendum consequat eros, ut rutrum erat congue sed.
+        Curabitur at rhoncus turpis. In luctus nec neque sed bibendum. Curabitur volutpat leo ut fermentum dictum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam nunc ac leo commodo, nec fringilla
+        augue viverra. Suspendisse eleifend a ipsum ac imperdiet. Ut faucibus sollicitudin viverra. Sed id dapibus
+        ligula. Fusce porta eros et arcu efficitur rhoncus. Donec bibendum consequat eros, ut rutrum erat congue sed.
+        Curabitur at rhoncus turpis. In luctus nec neque sed bibendum. Curabitur volutpat leo ut fermentum dictum.
+      </p>
+      <img src="/images/example_image.png" style="max-width:600px;">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam nunc ac leo commodo, nec fringilla
+        augue viverra. Suspendisse eleifend a ipsum ac imperdiet. Ut faucibus sollicitudin viverra. Sed id dapibus
+        ligula. Fusce porta eros et arcu efficitur rhoncus. Donec bibendum consequat eros, ut rutrum erat congue sed.
+        Curabitur at rhoncus turpis. In luctus nec neque sed bibendum. Curabitur volutpat leo ut fermentum dictum.
+      </p>
     </div>
   </div>
 </template>
+
+<script lang="ts">
+// Dependencies
+import { defineComponent } from "vue";
+// Declarations
+declare const MathJax: { typeset: () => void }
+
+export default defineComponent({
+  name: "AboutView",
+  mounted() {
+    MathJax.typeset();
+  },
+  updated() {
+    MathJax.typeset();
+  }
+});
+</script>
 
 <style lang="scss" scoped>
 @use "@/assets/styles/engenuity_scaling_system.scss" as scale;
@@ -17,8 +145,34 @@
 }
 
 .about-contents {
-  display: flex;
   width: 100%;
   max-width: scale.$max-width;
+  padding: 0em scale.size("xxh");
+}
+
+.about-contents :first-child {
+  margin-top: 0em;
+}
+
+.about-contents :last-child {
+  margin-bottom: 0em;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+li,
+ol,
+p {
+  margin: scale.size("s") 0em;
+}
+
+img {
+  display: block;
+  width: 100%;
+  margin: scale.size("xxl") auto;
 }
 </style>
